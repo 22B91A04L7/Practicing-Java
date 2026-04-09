cef72294575e8bcc2aa3d5eb3955434541d5c74c
Venkat Sunkara <venkatsunkara9959@gmail.com>
2026-04-09 14:34:23 +0530

    parameterised constructor

---
+++ parametersied constructor/Main.java
@@ -0,0 +1,27 @@
+class Car {
+    String name;
+    int price;
+
+    // Default constructor
+    public Car() {
+        this.name = "Not defined";
+        this.price = 0;
+        System.out.println("Default Constructor Called");
+    }
+
+    // parameterised constructor
+    public Car(String name, int price) {
+        this.name = name;
+        this.price = price;
+        System.out.println("Parametrized Constructor Called");
+    }
+
+}
+
+public class Main {
+    public static void main(String args[]) {
+        // Car obj1 = new Car();
+
+        Car obj2 = new Car("Ferari", 50000);
+    }
+}
